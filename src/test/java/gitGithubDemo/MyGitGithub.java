8553c4c7ad06d390bba912a8ee974372f3678f96
JavedTiktok <javeedtiktok@gmail.com>
2022-09-05 01:40:21 -0400

    first version

---
+++ src/test/java/gitGithubDemo/MyGitGithub.java
@@ -0,0 +1,19 @@
+package gitGithubDemo;
+
+public class MyGitGithub {
+
+
+    public static void main(String[] args) {
+        MyGitGithub obj = new MyGitGithub();
+
+
+        String s1 = "javeed";
+        String s2 = "javeed";
+        if (s1 == s2) {
+            System.out.println("this is real name");
+        } else {
+            System.out.println("this is fake name");
+
+        }
+    }
+}
